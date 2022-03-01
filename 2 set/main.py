15489fecfca6d5cfb8487dddcea208bf1e04537e
Valerii Matveev <82083854+MatveevValerii@users.noreply.github.com>
2022-03-01 14:40:26 +0300

    modeling

--- 2 set/main.py
+++ 2 set/main.py
@@ -1,46 +1,41 @@
 from scipy import stats 
 import numpy as np
 import matplotlib.pyplot as mpl
+from scipy.stats import linregress
 
-with open("data1.csv") as data_file:
-    array = np.loadtxt(data_file, delimiter=" ")
+Bl_pres= ([1, 0, 1, 2, 5, 1, 4, 6, 2, 3], [5, 4, 6, 8, 4, 5, 7, 9, 7, 6])
+Noise = ([60, 63, 65, 70, 70, 70, 80, 90, 80, 80], [85, 89, 90, 90, 90, 90, 94, 100, 100, 100])
 
-res = stats.describe(array)
+Bl_pres =np.array(Bl_pres)
+Noise = np.array(Noise)
 
-print('Average of results', res.mean)
-print('Standart deviation', stats.tstd(array))
-print('Range of variables \n', res.minmax[1]-res.minmax[0])
+Bl_pres = np.reshape(Bl_pres, 20)
+Noise = np.reshape(Noise, 20)
 
 
-start = 0
-end = 120
+
+
 step = 1
 
-x = np.arange(start, end, step)
-for i in range(0,6):
-    k=i+1
-    mpl.plot(x,array[:,i], label=('variable '+str(k)))
-    #mpl.plot(x,array[:,i], label=(k, 'variable'))
-    mpl.xlabel('Row number')
-    mpl.ylabel('Variable value')
-    mpl.legend()
-    mpl.grid()
-    mpl.show()
-    
+x= np.arange(0, len(Noise), step)
 
-#c.	Create a scatterplots of pairs of  variables.
+y = np.column_stack((Noise, Bl_pres))
+#y = np.reshape(y, ((3, 20)), 'A')
+y=np.transpose(y)
+r = np.corrcoef(y, rowvar=True)
 
-mpl.scatter(array[:,0], array[:,1], s=6, label="1 and 2 variables")
+
+line = stats.linregress(Noise, Bl_pres)
+#print(f'y={line.slope:.3f}x {line.intercept:.3f}') formula of the line
+
+mpl.scatter(Noise, Bl_pres, s=8, label="Blood pressure vs noise")
+mpl.plot(Noise, line.slope*Noise + line.intercept, 'tab:orange', label = 'Fitted line')
 mpl.legend()
 mpl.grid()
 mpl.show()
 
-mpl.scatter(array[:,2], array[:,3], s=6, label="3 and 4 variables")
-mpl.legend()
-mpl.grid()
-mpl.show()
+Noise_level = 90
+print('Matrice of corr coefficients is \n', r)
+print('Answer to part e (noise level=90) ',round(line.slope*Noise_level+line.intercept,4))
 
-mpl.scatter(array[:,4], array[:,5], s=6, label="5 and 6 variables")
-mpl.legend()
-mpl.grid()
-mpl.show()
+print('Answer to part f', (5-line.intercept)/line.slope)
